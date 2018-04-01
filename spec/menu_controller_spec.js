a17efd67bae7286a43968143f7325e51854e8c37
Kwena Mokgohlwa <kwena@Kwenas-MacBook-Air.local>
2018-04-02 01:25:38 +0200

    Checkpoint Listing and Searching Complete

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -6,9 +6,5 @@ describe("MenuController", () => {
     this.menu = new MenuController();
   });
 
-  describe("#remindMe()", () => {
-    it('should return \"Learning is a life-long pursuit\"', () => {
-      expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit");
-    });
-  });
+
 });
